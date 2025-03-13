6e5d262ca5fff79505054915365d041d4c45ab7f
Matt Pardini <matt.pardini@icloud.com>
2025-03-13 14:08:45 -0400

    Initial Commit

---
+++ TestAppGit/TestAppGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppGitApp.swift
+//  TestAppGit
+//
+//  Created by Matthew Pardini on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppGitTests/TestAppGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppGitTests.swift
+//  TestAppGitTests
+//
+//  Created by Matthew Pardini on 3/13/25.
+//
+
+import Testing
+@testable import TestAppGit
+
+struct TestAppGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
