c3aee19b36832a043882eca9811dce0df04be162
mpardini <matt.pardini@icloud.com>
2025-03-13 14:21:20 -0400

    jjfjfj

--- TestAppGit/ContentView.swift
+++ TestAppGit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, my world!")
+            Text("Hello, my big world!")
         }
         .padding()
     }
