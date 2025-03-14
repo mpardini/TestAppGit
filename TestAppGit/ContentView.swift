b20f8836044198f0f70786df4d585c8d9c142107
Matt Pardini <matt.pardini@icloud.com>
2025-03-13 22:35:04 -0400

    wefwfw

--- TestAppGit/ContentView.swift
+++ TestAppGit/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "triangle")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, my big world!")
+        
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+        
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("niagarafalls")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack {
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                //developer.apple.com/sf-symbols
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+
+                        
+                        Text("(Reviews 300)")
+                            .font(.caption)
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                }
+                
+                Text("Come visit the falls!")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+                
+                
+                    
+            }
+            
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15))
+            .padding()
         }
-        .padding()
+        
+        
     }
 }
 
